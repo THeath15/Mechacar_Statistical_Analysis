e1fe59ac3e45aa0ab6da2135ce0eb44454144e0c
Theresa  Heath <92903447+THeath15@users.noreply.github.com>
2022-02-19 16:07:15 -0600

    Final - deliverable 1 to 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -15,3 +15,12 @@ total_summary <- Suspension_Coil %>% summarize(Mean = mean(PSI), Median = median
 head(total_summary)
 # stat summary wtih group_by()
 lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI),Median = median(PSI), Variance = var(PSI), SD = sd(PSI) , .groups = 'keep')
+### Deliverable 3 ###
+# t-test for all the lots, pop mu = 1500 psi
+t.test(Suspension_Coil$PSI,mu=mean(Suspension_Coil$PSI))
+# t-test for lot 1
+t.test(subset(Suspension_Coil$PSI,Suspension_Coil$Manufacturing_Lot == "Lot1"),mu=mean(Suspension_Coil$PSI))
+# t-test for lot 2
+t.test(subset(Suspension_Coil$PSI,Suspension_Coil$Manufacturing_Lot == "Lot2"),mu=mean(Suspension_Coil$PSI))
+# t-test for lot 3
+t.test(subset(Suspension_Coil$PSI,Suspension_Coil$Manufacturing_Lot == "Lot3"),mu=mean(Suspension_Coil$PSI))
